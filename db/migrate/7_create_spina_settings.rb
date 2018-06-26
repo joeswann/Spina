50e554a6727030d0bd3610dd13d77fdd55c529a4
Joe Swann <joe@pixelbash.co.nz>
2018-06-26 19:01:43 +1200

    Fix > Update settings migration to use json (vs jsonb)
    Mysql 8.0 supports a table type of json but without a default

--- db/migrate/7_create_spina_settings.rb
+++ db/migrate/7_create_spina_settings.rb
@@ -2,7 +2,8 @@ class CreateSpinaSettings < ActiveRecord::Migration[5.0]
   def change
     create_table :spina_settings do |t|
       t.string :plugin
-      t.jsonb :preferences, default: {}
+      #t.jsonb :preferences, default: {}
+      t.json :preferences
       t.timestamps
     end
 
